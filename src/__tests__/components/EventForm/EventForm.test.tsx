ae5eb826fff89cb86bdd08a6595aa55dd2acf6f8
Denis Kolesnyov <Denis_Kolesnyov@epam.com>
2021-01-15 10:55:35 +0100

    writing tests + implementation for about sertion

--- src/__tests__/components/EventForm/EventForm.test.tsx
+++ src/__tests__/components/EventForm/EventForm.test.tsx
@@ -21,27 +21,93 @@ describe('EventForm', () => {
 
     test('should render event form', async () => {
         setupComponent(mockedProps);
-
         expect(await screen.findByTestId('event-form')).toBeInTheDocument();
     });
 
     test('should render "About" section', async () => {
         setupComponent(mockedProps);
-
         expect(await screen.findByText('About')).toBeInTheDocument();
     });
 
     test('should render "Coordinator" section', async () => {
         setupComponent(mockedProps);
-
         expect(await screen.findByText('Coordinator')).toBeInTheDocument();
     });
 
     test('should render "When" section', async () => {
         setupComponent(mockedProps);
-
         expect(await screen.findByText('When')).toBeInTheDocument();
     });
 
+    test('should render label and input for title', async () => {
+        setupComponent(mockedProps);
+        expect(await screen.findByText('Title')).toBeInTheDocument();
+        const input = screen.getByTestId('form-title');
+        expect(input).toBeInTheDocument();
+        expect(input.getAttribute('placeholder')).toBe('Make it short and clear');
+    });
+
+    test('should render label and input for description', async () => {
+        setupComponent(mockedProps);
+
+        expect(await screen.findByText('Description')).toBeInTheDocument();
+        const input = screen.getByTestId('form-description');
+        expect(input).toBeInTheDocument();
+        expect(input.getAttribute('placeholder')).toBe('Write about your event, be creative');
+        expect(screen.getByText('Max characters length')).toBeInTheDocument();
+        expect(screen.getByTestId('form-description-char-count')).toBeInTheDocument();
+    });
+
+    test('should render label and input for category', async () => {
+        setupComponent(mockedProps);
+        expect(await screen.findByText('Category')).toBeInTheDocument();
+        expect(screen.getByTestId('form-category')).toBeInTheDocument();
+    });
+
+    test('should render label and input for payment', async () => {
+        setupComponent(mockedProps);
+
+        expect(await screen.findByText('Payment')).toBeInTheDocument();
+        const input = screen.getByTestId('form-payment');
+        expect(input).toBeInTheDocument();
+    });
+
+    test('should render label and input for reward', async () => {
+        setupComponent(mockedProps);
+
+        expect(await screen.findByText('Reward')).toBeInTheDocument();
+        const input = (screen.getByTestId('form-reward'));
+        expect(input).toBeInTheDocument();
+        expect(input.getAttribute('placeholder')).toBe('Number');
+        expect(screen.getByText('reward points for attendance')).toBeInTheDocument();
+    });
+
+    // test('should render label and input for responsible', async () => {
+    //     setupComponent(mockedProps);
+
+    //     expect(await screen.findByText('')).toBeInTheDocument();
+    //     expect(screen.getByTestId('')).toBeInTheDocument();
+    // });
+
+    // test('should render label and input for email', async () => {
+    //     setupComponent(mockedProps);
+
+    //     expect(await screen.findByText('')).toBeInTheDocument();
+    //     expect(screen.getByTestId('')).toBeInTheDocument();
+    // });
+
+    // test('should render label and input for start date and time', async () => {
+    //     setupComponent(mockedProps);
+
+    //     expect(await screen.findByText('')).toBeInTheDocument();
+    //     expect(screen.getByTestId('')).toBeInTheDocument();
+    // });
+
+    // test('should render label and input for duration', async () => {
+    //     setupComponent(mockedProps);
+
+    //     expect(await screen.findByText('')).toBeInTheDocument();
+    //     expect(screen.getByTestId('')).toBeInTheDocument();
+    // });
 
 });
--- src/components/CreateUpdateEvent/EventForm.tsx
+++ src/components/CreateUpdateEvent/EventForm.tsx
@@ -1,10 +1,10 @@
 import React from "react";
 import { IActivity } from "../../models/activity.model";
-import { ICoordinator } from "../../models/coordinator.model";
 import { ICredentials } from "../../models/credentials.model";
 import { IEvent } from "../../models/event.model";
 import { IGroupedDropdown } from "../../models/inputs.model";
 import { Box } from "../styled/Box/Box";
+import { Text } from "../styled/Text";
 import { EventFormSection } from "./EventFormSection";
 
 export interface IEventFormProps {
@@ -16,7 +16,25 @@ export interface IEventFormProps {
 export const EventForm: React.FC<IEventFormProps> = ({ event, activities, coordinators }) => {
     return (
         <Box data-testid="event-form">
-            <EventFormSection title="About"></EventFormSection>
+            <EventFormSection title="About">
+                <Box>
+                    <label>Title<input data-testid="form-title" placeholder="Make it short and clear" /></label>
+                </Box>
+                <Box>
+                    <label>Description<textarea data-testid="form-description" placeholder="Write about your event, be creative" /></label>
+                    <Text>Max characters length</Text><Text data-testid="form-description-char-count"></Text>
+                </Box>
+                <Box>
+                    <label>Category<select data-testid="form-category" /></label>
+                </Box>
+                <Box>
+                    <label>Payment<Box data-testid="form-payment" /></label>
+                </Box>
+                <Box>
+                    <label>Reward<input data-testid="form-reward" placeholder="Number" /></label>
+                    <Text>reward points for attendance</Text>
+                </Box>
+            </EventFormSection>
             <EventFormSection title="Coordinator"></EventFormSection>
             <EventFormSection title="When"></EventFormSection>
         </Box>
