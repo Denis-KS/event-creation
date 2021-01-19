8e4659172e2261fee3633a70e1225237d61f5d26
Denis Kolesnyov <Denis_Kolesnyov@epam.com>
2021-01-19 09:37:10 +0100

    applying FormField vomponent

--- src/components/CreateUpdateEvent/FormSections/FormCoordinatorSection.tsx
+++ src/components/CreateUpdateEvent/FormSections/FormCoordinatorSection.tsx
@@ -1,16 +1,16 @@
 import React from "react";
-import { Box } from "../../styled/Box/Box";
+import { FormField } from "../FormInputs/FormField";
 import { EventFormSection } from "./EventFormSection";
 
 export const FormCoordinatorSection: React.FC = () => {
     return (
         <EventFormSection title="Coordinator">
-            <Box>
-                <label>Responsible<select data-testid="form-responsible" /></label>
-            </Box>
-            <Box>
-                <label>Email<input data-testid="form-email" placeholder="Email" /></label>
-            </Box>
+            <FormField title="Responsible">
+                <select data-testid="form-responsible" />
+            </FormField>
+            <FormField title="Email">
+                <input data-testid="form-email" placeholder="Email" />
+            </FormField>
         </EventFormSection>
     );
 }
--- src/components/CreateUpdateEvent/FormSections/FormGeneralSection.tsx
+++ src/components/CreateUpdateEvent/FormSections/FormGeneralSection.tsx
@@ -1,28 +1,29 @@
 import React from "react";
 import { Box } from "../../styled/Box/Box";
 import { Text } from "../../styled/Text";
+import { FormField } from "../FormInputs/FormField";
 import { EventFormSection } from "./EventFormSection";
 
 export const FormGeneralSection: React.FC = () => {
     return (
         <EventFormSection title="About">
-            <Box>
-                <label>Title<input data-testid="form-title" placeholder="Make it short and clear" /></label>
-            </Box>
-            <Box>
-                <label>Description<textarea data-testid="form-description" placeholder="Write about your event, be creative" /></label>
+            <FormField title="Title">
+                <input data-testid="form-title" placeholder="Make it short and clear" />
+            </FormField>
+            <FormField title="Description">
+                <textarea data-testid="form-description" placeholder="Write about your event, be creative" />
                 <Text>Max characters length</Text><Text data-testid="form-description-char-count"></Text>
-            </Box>
-            <Box>
-                <label>Category<select data-testid="form-category" /></label>
-            </Box>
-            <Box>
-                <label>Payment<Box data-testid="form-payment" /></label>
-            </Box>
-            <Box>
-                <label>Reward<input data-testid="form-reward" placeholder="Number" /></label>
+            </FormField>
+            <FormField title="Category">
+                <select data-testid="form-category" />
+            </FormField>
+            <FormField title="Payment">
+                <Box data-testid="form-payment" />
+            </FormField>
+            <FormField title="Reward">
+                <input data-testid="form-reward" placeholder="Number" />
                 <Text>reward points for attendance</Text>
-            </Box>
+            </FormField>
         </EventFormSection>
     );
 }
